af9459cbbf4197a31be85cee47b223861f27a242
Alexandra-Irina ANDREI (119498) <alexandra.andrei01@stud.acs.upb.ro>
2023-04-08 15:01:43 +0300

    tabla de sah

---
+++ Chess/Pages/Home.cshtml.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System;
+namespace Chess.Pages.Shared
+{
+    public class HomeModel : PageModel
+    {
+        public void OnGet()
+        { }
+    }    
+
+
+        }
+
+    
+
+
